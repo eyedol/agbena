6cc06d6a2e69f94f06c40c02ef74bd80ae80219b
Henry Addo <eyedol@addhen.(none)>
2010-02-22 18:28:28 +0000

    Added chat details

---
+++ application/views/chat.php
@@ -0,0 +1,36 @@
+<?php 
+/**
+ * Help view page.
+ *
+ * PHP version 5
+ * LICENSE: This source file is subject to LGPL license 
+ * that is available through the world-wide-web at the following URI:
+ * http://www.gnu.org/copyleft/lesser.html
+ * @author     Ushahidi Team <team@ushahidi.com> 
+ * @package    Ushahidi - http://source.ushahididev.com
+ * @module     API Controller
+ * @copyright  Ushahidi - http://www.ushahidi.com
+ * @license    http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License (LGPL) 
+ */
+?>
+
+<div id="content">
+	<div class="content-bg">
+		<!-- start reports block -->
+		<div class="big-block">
+			<h1>Chat</h1>
+			<p>If you wish to chat with people in the community in real-time, to share ideas or discuss about issues, you are welcome to join our community IRC channel: <strong>#gh-dev</strong> on irc.freenode.net.<br /><br /><br /></p>
+
+<p>There is a link to a web-based chat available below which allow access to this same chat room without needing an IRC client. To enter the room, please give yourself a nickname and specify the <strong>#gh-dev</strong> channel then click CONNECT to join the chatroom.<br /><br /><br /></p>
+
+<p>You can also join GDC mailing list for email conversation at <a href="http://groups.google.com/group/ghanadevelopers">http://groups.google.com/group/ghanadevelopers</a><br /><br /><br /></p>
+
+<p>Connect to the chatroom here: <a href="http://webchat.freenode.net">http://webchat.freenode.net</a><br /><br /><br /></p>				
+		</div>
+		<!-- end reports block -->
+	</div>
+</div>
+			</div>
+		</div>
+	</div>
+				
